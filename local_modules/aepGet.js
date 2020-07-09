c18657c4bafca649893f0c700e2d1af49c2f9f6d
justjcurtis <justjcurtis@gmail.com>
2020-07-09 20:11:56 +0100

    initial implementation of parallel download aggregation

--- local_modules/aepGet.js
+++ local_modules/aepGet.js
@@ -20,10 +20,29 @@ const aepGet = async (egID, args) =>{
         downloadTasks.push(getErrorResponse(egID, errorGroupResponse[i], args.key, args.owner, args.app))
     }
     console.log('Finished.')
+
     console.log('Downloading individual error responses')
     progress.start(errorGroupResponse.length, 0);
     let downloadResults = await Promise.allSettled(downloadTasks);
     progress.stop();
+    console.log('Finished.')
+
+    console.log('Injecting individual error responses')
+    const inject = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    inject.start(errorGroupResponse.length, 0);
+    for(let i = 0; i < errorGroupResponse.length; i++){
+        let errorID = errorGroupResponse[i].errorId;
+        for(let j = 0; j < downloadResults.length; j++){
+            if(downloadResults[j].errorId == errorID){
+                errorGroupResponse[i].reasonFrames = downloadResults[j].reasonFrames
+                errorGroupResponse[i].carrierName = downloadResults[j].carrierName
+                errorGroupResponse[i].jailbreak = downloadResults[j].jailbreak
+                errorGroupResponse[i].properties = downloadResults[j].properties
+            }
+        }
+        inject.update[i+1]
+    }
+    inject.stop()
     console.log('Finished.')
 
     console.log(`Writing output to ${args.output}`)
@@ -38,6 +57,7 @@ const getErrorResponse = async (groupID, errorID, key, owner, app) => {
     }
     progress.update(i+1);
     return {
+        'errorId':errorResponse.errorId,
         'reasonFrames':errorResponse.reasonFrames,
         'carrierName':errorResponse.carrierName,
         'jailbreak':errorResponse.jailbreak,
