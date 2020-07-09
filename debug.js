487f00b8a978d47e564cd2683be0337cfb3678eb
justjcurtis <justjcurtis@gmail.com>
2020-07-09 21:28:48 +0100

    removed apikey from debug.js

--- debug.js
+++ debug.js
@@ -2,10 +2,10 @@ const {
     aepGet,
 } = require('./index.js')
 
-let key = "707f847e66f22a5068880f2d83b87da881bcf92f"
+let key = "apikey"
 let owner = "ideagen-plc"
 let app = "Coruson-Mobile-iOS"
-let output = "/Users/jacson.curtis/errors.json"
+let output = "~/errors.json"
 let from = "2020%2F04%2F10"
 let to = "2020%2F07%2F09"
 let ErrorGroupId = "2491346219"
