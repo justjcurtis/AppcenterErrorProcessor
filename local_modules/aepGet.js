7d9ef0bd6a5f8215365236be324ee99bccbe59a3
justjcurtis <justjcurtis@gmail.com>
2020-07-09 21:11:23 +0100

    implemented chunking

--- local_modules/aepGet.js
+++ local_modules/aepGet.js
@@ -20,36 +20,78 @@ const aepGet = async (egID, args) =>{
     }
     console.log('Finished.')
 
-    console.log('Enumerating individual error responses')
-    const enumerate = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-    enumerate.start(errorGroupResponse.length, 0);
 
-    let downloadTasks = []
+    console.log('Chunking workload...')
+    const chunking = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    chunking.start(errorGroupResponse.length, 0);
+    let chunks = [[]]
+    let chunksize = 50000;
     for(let i = 0; i < errorGroupResponse.length; i++){
-        downloadTasks.push(getErrorResponse(egID, errorGroupResponse[i], args.key, args.owner, args.app))
+        if((i+1)%chunksize == 0){
+            chunks.push([])
+        }
+        chunks[chunks.length-1].push(errorGroupResponse[i]);
+        chunking.update(i+1)
+    }
+    chunking.stop()
+    console.log('Finished.')
+    errorGroupResponse = undefined;
+
+    resultingChunks = [];
+    for(let i = 0; i< chunks.length; i++){
+        console.log(`Processing chunk ${i+1} of ${chunks.length}`)
+        resultingChunks.push(await processChunk(chunks[i].slice(0), egID, args));
+    }
+    chunks = undefined
+
+    console.log('Aggregating chunks...');
+    const aggregate = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    aggregate.start(resultingChunks.length, 0);
+    let result = []
+    for(let i = 0; i< resultingChunks.length; i++){
+        for(let j = 0; j< resultingChunks[i].length; j++){
+            result.push(resultingChunks[i][j]);
+        }
+        aggregate.update(i+1);
+    }
+    aggregate.stop();
+    console.log('Finished.')
+
+    console.log(`Writing output to ${args.output}...`)
+    writeFile(JSON.stringify(errorGroupResponse), args.output)
+    console.log('Finished.')
+}
+
+const processChunk = async (chunk, egID, args) =>{
+    console.log('Enumerating individual error responses...')
+    const enumerate = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    enumerate.start(chunk.length, 0);
+    let downloadTasks = []
+    for(let i = 0; i < chunk.length; i++){
+        downloadTasks.push(getErrorResponse(egID, chunk[i], args.key, args.owner, args.app))
         enumerate.update(i+1)
     }
     enumerate.stop()
     console.log('Finished.')
 
-    console.log('Downloading individual error responses')
-    progress.start(errorGroupResponse.length, 0);
+    console.log('Downloading individual error responses...')
+    progress.start(chunk.length, 0);
     let downloadResults = await Promise.allSettled(downloadTasks);
     progress.stop();
     console.log('Finished.')
 
-    console.log('Injecting individual error responses')
+    console.log('Injecting individual error responses...')
     const inject = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-    inject.start(errorGroupResponse.length, 0);
-    for(let i = 0; i < errorGroupResponse.length; i++){
-        let errorID = errorGroupResponse[i].errorId;
+    inject.start(chunk.length, 0);
+    for(let i = 0; i < chunk.length; i++){
+        let errorID = chunk[i].errorId;
         for(let j = 0; j < downloadResults.length; j++){
             if(downloadResults[j]!= undefined){
                 if(downloadResults[j].errorId == errorID){
-                    errorGroupResponse[i].reasonFrames = downloadResults[j].reasonFrames
-                    errorGroupResponse[i].carrierName = downloadResults[j].carrierName
-                    errorGroupResponse[i].jailbreak = downloadResults[j].jailbreak
-                    errorGroupResponse[i].properties = downloadResults[j].properties
+                    chunk[i].reasonFrames = downloadResults[j].reasonFrames
+                    chunk[i].carrierName = downloadResults[j].carrierName
+                    chunk[i].jailbreak = downloadResults[j].jailbreak
+                    chunk[i].properties = downloadResults[j].properties
                 }
             }
         }
@@ -57,10 +99,7 @@ const aepGet = async (egID, args) =>{
     }
     inject.stop()
     console.log('Finished.')
-
-    console.log(`Writing output to ${args.output}`)
-    writeFile(JSON.stringify(errorGroupResponse), args.output)
-    console.log('Finished.')
+    return chunk;
 }
 
 const getErrorResponse = async (groupID, errorID, key, owner, app) => {
